455e286314eeb6dc155a7a7534fda6e4e44ad789
Jacques Camier <jcamier@gmail.com>
2020-08-06 19:35:38 -0500

    add dotenv

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +24,7 @@ POLLS_DIR = os.path.join(BASE_DIR, "polls", "templates", "polls")
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'makeSureYouDontUseThisKeyInProduction'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
